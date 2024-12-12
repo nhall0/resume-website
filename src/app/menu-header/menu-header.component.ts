21d9da8f1c85d16a04f0593c3f288c64158eab77
Niklas <niklashallstein1798@gmail.com>
2024-12-11 19:44:31 -0500

    menu-header

---
+++ src/app/menu-header/menu-header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItemComponent } from '../menu-item/menu-item.component';
+import { MenuItem } from '../services/menu.model';
+import { NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-menu-header',
+  standalone: true,
+  imports: [MenuItemComponent,
+            NgFor,
+            NgIf],
+  templateUrl: './menu-header.component.html',
+  styleUrl: './menu-header.component.scss'
+})
+export class MenuHeaderComponent implements OnInit {
+  menuItems: MenuItem[] = [
+    { title: 'home', route: '/', icon: 'home' },
+    { title: 'projects', route: '/projects', icon: 'info' },
+    { title: 'resume', route: '/resume', icon: 'email' },
+    { title: 'contact', route: '/contact', icon: 'contact' }
+  ];
+
+  constructor() { }
+
+  isLastItem(item: MenuItem) : boolean {
+    return this.menuItems.indexOf(item) === this.menuItems.length - 1;
+  }
+
+  ngOnInit(): void {}
+}
---
+++ src/app/menu-item/menu-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { MenuItem } from '../services/menu.model';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-menu-item',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './menu-item.component.html',
+  styleUrl: './menu-item.component.scss'
+})
+export class MenuItemComponent implements OnInit {
+  @Input() item : MenuItem | null = null;
+
+  @Output() onClick : EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {}
+}
